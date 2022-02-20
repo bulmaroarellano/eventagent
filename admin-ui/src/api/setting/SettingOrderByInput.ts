3e2ec03eb23f798fbcef126c4ff4a287b6312a25
Bulmaro Arellano <67934295+bulmaroarellano@users.noreply.github.com>
2022-02-20 00:36:11 -0600

    amplication-build-ckzuwc22z64993001s6r6rd7clt

---
+++ admin-ui/src/api/setting/SettingOrderByInput.ts
@@ -0,0 +1,9 @@
+import { SortOrder } from "../../util/SortOrder";
+
+export type SettingOrderByInput = {
+  createdAt?: SortOrder;
+  id?: SortOrder;
+  itemm?: SortOrder;
+  updatedAt?: SortOrder;
+  value?: SortOrder;
+};
