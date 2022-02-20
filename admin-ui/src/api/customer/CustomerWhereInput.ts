e7d00e520a084c52528fd4c7ec1ede777f254147
Bulmaro Arellano <67934295+bulmaroarellano@users.noreply.github.com>
2022-02-20 00:37:26 -0600

    amplication-build-ckzuwdnev127320601s6zlobo5i7

---
+++ admin-ui/src/api/customer/CustomerWhereInput.ts
@@ -0,0 +1,12 @@
+import { AddressWhereUniqueInput } from "../address/AddressWhereUniqueInput";
+import { StringNullableFilter } from "../../util/StringNullableFilter";
+import { StringFilter } from "../../util/StringFilter";
+
+export type CustomerWhereInput = {
+  addeessId?: AddressWhereUniqueInput;
+  email?: StringNullableFilter;
+  firstName?: StringNullableFilter;
+  id?: StringFilter;
+  lastName?: StringNullableFilter;
+  phone?: StringNullableFilter;
+};
